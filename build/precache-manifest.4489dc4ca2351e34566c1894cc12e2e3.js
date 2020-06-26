a485942e9d4d6a1083f01ae30617ea16d2b58f72
taelee <taelee@student.42seoul.kr>
2020-06-26 09:00:46 +0900

    add build folder

---
+++ build/precache-manifest.4489dc4ca2351e34566c1894cc12e2e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040c31b9bd4fcfa16c9c17bf775d447f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db52385e8f476d255d39",
+    "url": "/static/css/main.77c3d8a6.chunk.css"
+  },
+  {
+    "revision": "75d39332a90a3b7b704c",
+    "url": "/static/js/2.4977a951.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4977a951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db52385e8f476d255d39",
+    "url": "/static/js/main.57e86a6c.chunk.js"
+  },
+  {
+    "revision": "2e14c61f8cb3c7cfa6ea",
+    "url": "/static/js/runtime-main.8b4fba76.js"
+  },
+  {
+    "revision": "a09b981735a2d78182f6baaaa0babb5a",
+    "url": "/static/media/NEXON Lv2 Gothic Bold.a09b9817.ttf"
+  }
+]);
